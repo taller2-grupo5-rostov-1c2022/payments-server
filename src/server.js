531fdf4ac1bb473e2a2889325895c7a21fce5901
Facundo Mastricchio <facundomastricchio@gmail.com>
2022-05-23 00:44:39 -0300

    build: add docker-compose

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ routes.forEach(route => fastify.route(route({ config, services })));
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(3000, "0.0.0.0");
+    await fastify.listen(process.env.PORT || 3000, "0.0.0.0");
     fastify.log.info(`server listening on ${fastify.server.address().port}`);
   } catch (err) {
     fastify.log.error(err);
