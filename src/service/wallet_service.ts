d29e6e37cfe7795b259e957f1c21557067df49c0
Facundo Mastricchio <facundomastricchio@gmail.com>
2022-05-25 00:55:30 -0300

    feat: add base deposit model

--- src/service/wallet_service.ts
+++ src/service/wallet_service.ts
@@ -1,6 +1,6 @@
 import { ApiError } from "../errors";
 import { Wallet } from "../models/wallet";
-import * as WalletRepository from "../postgres/repositories/wallet";
+import * as WalletRepository from "../postgres/repositories/wallet_repository";
 
 export const findAll = async (): Promise<Wallet[] | ApiError> =>
   wrapWithUnknownError(() => WalletRepository.findAll(), "Unable to find all wallets due to unknown error");
@@ -14,7 +14,7 @@ export const findById = async (walletId: number): Promise<Wallet[] | ApiError> =
 export const create = async (newWalletDto: Wallet): Promise<Wallet | ApiError> =>
   wrapWithUnknownError(
     () => WalletRepository.create(newWalletDto),
-    `Unable to create wallet ${newWalletDto.id} due to unknown error`,
+    `Unable to create wallet ${newWalletDto} due to unknown error`,
   );
 
 const wrapWithUnknownError = (process: () => Promise<any>, message: string) =>
