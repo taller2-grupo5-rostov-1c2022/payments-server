0faae1ffe763ddd543d38c657c1852ad3a25d98e
Nicolas Zulaica <66538092+NicolasEzequielZulaicaRivera@users.noreply.github.com>
2022-06-12 18:51:16 -0300

    update balance header user-id -> uid

--- src/handlers/getBalanceHandler.js
+++ src/handlers/getBalanceHandler.js
@@ -3,21 +3,21 @@ function schema() {
     headers: {
       type: "object",
       properties: {
-        "user-id": {
+        "uid": {
           type: "string",
         },
       },
     },
-    required: ["user-id"],
+    required: ["uid"],
   };
 }
 
 function handler({ walletService }) {
   return async function (req, reply) {
-    const userId = req.headers["user-id"];
+    const userId = req.headers["uid"];
     const balanceInEthers = await walletService.getBalanceByUserId(userId);
     const code = !balanceInEthers ? 404 : 200;
-    const body = !balanceInEthers ? { message: `Unable to find wallet with provided user id ${userId}` } : { 'balance': balanceInEthers };
+    const body = !balanceInEthers ? { message: `Unable to find wallet with provided uid ${userId}` } : { 'balance': balanceInEthers };
     return reply.code(code).send(body);
   };
 }
